c361bb67856a191293383d98ec1fe5c076fc7b19
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2025-01-22 14:34:22 +0100

    adding cmakelists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <opencv2/gapi.hpp>
 
 using namespace cv;
 
@@ -7,7 +9,7 @@ int main()
 {
     Mat image;
     image = cv::imread("wallhaven-o5ov3l.jpg");
-    namedWindow("Goddamn Hippies", WINDOW_AUTOSIZE );
+    namedWindow("Goddamn Hippies", WINDOW_NORMAL );
     imshow("Goddamn Hippies", image );
 
     waitKey(0);
